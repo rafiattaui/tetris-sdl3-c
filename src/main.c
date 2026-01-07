c85f704babe0136a726b497bdf5d22c369d1aa67
rafiattaui <athallah.rafi13@gmail.com>
2026-01-07 15:06:14 +0700

    draw_grid func.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "SDL3/SDL_rect.h"
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -7,9 +8,40 @@ static SDL_Renderer* gRenderer = NULL;
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
-#define GRID_SIZE 30
-#define BOARD_WIDTH 10
-#define BOARD_HEIGHT 20
+#define BLOCK_SIZE 30 // 30 pixels per block
+#define BOARD_WIDTH 10 // 10 blocks wide
+#define BOARD_HEIGHT 20 // 20 blocks high
+
+int blocks[BOARD_WIDTH][BOARD_HEIGHT] = {0}; // 0 = no block, 1 = block
+
+void draw_grid(SDL_Renderer *renderer)
+{
+    const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
+    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
+    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
+    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
+
+    for (int row = 0; row < BOARD_HEIGHT; row++){
+        for (int col = 0; col < BOARD_WIDTH; col++){
+            SDL_FRect cell = {
+                .x = (float) col * BLOCK_SIZE,
+                .y = (float) row * BLOCK_SIZE,
+                .w = (float) BLOCK_SIZE,
+                .h = (float) BLOCK_SIZE
+            };
+
+            SDL_RenderRect(renderer, &cell);
+        }
+    }
+}
+
+void draw_pieces(SDL_Renderer *renderer)
+{
+    
+}
+
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -41,15 +73,11 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
-    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
-    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(gRenderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
-
+    SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
     /* clear the window to the draw color. */
     SDL_RenderClear(gRenderer);
+
+    draw_grid(gRenderer);
 
     /* put the newly-cleared rendering on the screen. */
     SDL_RenderPresent(gRenderer);
